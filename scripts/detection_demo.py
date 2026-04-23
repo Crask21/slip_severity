63cb858aca3c90a7e0c3c205c5eb183c4979a981
Crask21 <114492059+Crask21@users.noreply.github.com>
2026-04-23 16:23:09 +0200

    demo v1

--- scripts/detection_demo.py
+++ scripts/detection_demo.py
@@ -1,7 +1,27 @@
-
-#import the five classifiers
+#Following code shows the use of the five classifiers for slip detection.
+import csv
+import numpy as np
 import joblib
-classifiers = {}
-for finger in ['thumb', 'index', 'middle', 'ring', 'little']:
-    classifiers[finger] = joblib.load(f'{finger}_classifier.pkl')
+import os
+#jump one directory up to access the model files
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model_path = os.path.join(root_path, "models", "detection_rf_ff.pkl")
+detection_classifier = joblib.load(model_path)
+
+
+data_path = os.path.join(root_path, "data", "non_contact.csv")
+import pandas as pd
+test_data = pd.read_csv(data_path)
+
+
+fingers = []
+for i in range(5):
+    finger_data = test_data.iloc[0][1+i*51:1+(i+1)*51].values.astype(float)
+    fingers.append(finger_data)
+fingers = np.array(fingers)
+
+for i in range(fingers.shape[0]):
+    finger_data = fingers[i].reshape(1, -1)
+    prediction = detection_classifier.predict(finger_data)
+    print(f"Finger {i+1} prediction: {prediction[0]}")
 
